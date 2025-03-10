34b9f5dcdc525a2d6b1172974e93418b522d02a1
Drake-Programming <adaptivecreature1864@gmail.com>
2025-03-09 23:20:39 -0400

    Added interface IBorrowable methods

--- src/Book.java
+++ src/Book.java
@@ -1,6 +1,7 @@
-public class Book extends Item {
+public class Book extends Item implements IBorrowable {
     private final String author;
     private final String ISBN;
+    private String borrower;
 
     public String getAuthor() { return author; }
     public String getISBN() { return ISBN; }
@@ -9,5 +10,30 @@ public class Book extends Item {
         super(title, publicationYear);
         this.author = author;
         this.ISBN = ISBN;
+        this.borrower = null;
+    }
+
+    @Override
+    public void borrowItem(String borrower) {
+        if (this.borrower == null) {
+            this.borrower = borrower;
+        }
+        else {
+            System.out.println(getTitle()+ " is already borrowed.");
+        }
+    }
+
+    @Override
+    public boolean isBorrowed() {
+        return this.borrower != null;
+    }
+
+    @Override
+    public void returnItem() {
+        if (isBorrowed()) {
+            this.borrower = null;
+        } else {
+            System.out.println(getTitle()+ " is not borrowed.");
+        }
     }
 }
